9588efe74a3f91318b3a8486196e9b7ae3eac265
Felix Menzel <36296555+FelixMZ2018@users.noreply.github.com>
2020-08-25 15:32:20 +0200

    Update dashboards_controller.rb

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -16,11 +16,8 @@ class DashboardsController < ApplicationController
         #### TO DO ADD FALLBACK LOGIC 
         @user = current_user
         url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{@user.user_preference.latitude}&lon=#{@user.user_preference.longitude}&exclude={minutely}&appid=#{ENV['OPENWEATHERAPI']}&units=metric"
-        p url
         response = open(url).read
-        p response
         hash = JSON.parse response
-        p hash
         return hash
 
 
