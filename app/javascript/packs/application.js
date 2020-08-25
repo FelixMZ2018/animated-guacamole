beee0fd2676795c3c6ecb4f4f202f1e8c06ec78a
Minh Vi Nguyen <minhvi@hotmail.de>
2020-08-25 15:57:21 +0200

    frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,6 +29,37 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
+  $('i').on('click', function(){
+
+  if($(this).text() == 'done'){
+    return false;
+  }
+
+  $(this).addClass('animating');
+
+  var activeStep = $('.step.active');
+  activeStep.addClass('sliding-out');
+  var nextStep = activeStep.next('.step');
+  nextStep.addClass('sliding-in');
+  nextStep.on('animationend', function(){
+    $(this).off('animationend');
+    activeStep.removeClass('active sliding-out').addClass('previous');
+    $(this).removeClass('next sliding-in').addClass('active');
+    $('i').removeClass('animating');
+
+    if(!$(this).next('.step').length){
+      $('i').html('done');
+    }
+    else {
+      $(this).next('.step').addClass('next');
+    }
+
+  })
+
+})
+
+
+Resources
   // Call your functions here, e.g:
   // initSelect2();
 });
