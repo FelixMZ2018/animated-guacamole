9ce53342ba24539dd7c45cafd23682173ee225de
Felix Menzel <FelixinLondon@googlemail.com>
2020-08-26 12:02:31 +0200

    Adddedpage

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :dashboards
+
+  get 'forecast', :to => 'dashboards#forecast'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
