91be42a0efb67ae3cc7eff6557c3873634d3f082
Minh Vi Nguyen <minhvi@hotmail.de>
2020-08-27 16:33:30 +0200

    customize forecast

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,9 +7,9 @@ class DashboardsController < ApplicationController
         @forecast = api_call()
         @forecast_current = @forecast['current']
         @forecast_hourly = []
-        count = 2 
+        count = 2
         @forecast['hourly'].each do |forecast|
-            count = count + 1 
+            count = count + 1
             if count == 3
                 @forecast_hourly << forecast
                 count = 0
@@ -25,11 +25,12 @@ class DashboardsController < ApplicationController
         @forecast_daily = @forecast['daily']
 
 
+
     end
     private
 
     def api_call
-        #### TO DO ADD FALLBACK LOGIC 
+        #### TO DO ADD FALLBACK LOGIC
         @user = current_user
         url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{@user.user_preference.latitude}&lon=#{@user.user_preference.longitude}&exclude={minutely}&appid=#{ENV['OPENWEATHERAPI']}&units=metric"
         response = open(url).read
