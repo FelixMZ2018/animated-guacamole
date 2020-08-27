b4c5f4db83b942a5d5be993139f95c5ab005302f
Felix Menzel <36296555+FelixMZ2018@users.noreply.github.com>
2020-08-27 11:50:26 +0200

    Update user_preferences_controller.rb

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -19,7 +19,8 @@ class UserPreferencesController < ApplicationController
     private
 
     def preferences_params
-        params.require(:user_preference).permit(:name, :photos, :description, :instructions)
+        params.require(:user_preference).permit(:city,:temp_br1,:temp_br2,:temp_br3,:temp_br4,:notification_time,:name )
+
 
         
     end
