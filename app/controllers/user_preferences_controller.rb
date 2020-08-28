1a038281c29c08ff0830f5c314887ee5e360ad8b
Felix <FelixinLondon@googlemail.com>
2020-08-28 16:38:38 +0200

    Changecity

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -20,6 +20,6 @@ class UserPreferencesController < ApplicationController
   private
 
   def preferences_params
-    params.require(:user_preference).permit(:city, :temp_br1, :temp_br2, :temp_br3, :temp_br4, :notification_time, :name, :avatar)
+    params.require(:user_preference).permit(:city, :temp_br1, :temp_br2, :temp_br3, :temp_br4, :notification_time, :name, :avatar, :address)
   end
 end
