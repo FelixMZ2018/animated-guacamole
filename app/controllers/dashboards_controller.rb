7aef8ac28320cea32d08db0bc624d253b532933c
Felix <FelixinLondon@googlemail.com>
2020-08-31 13:47:28 +0200

    Added if statement with redirect to settings page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,32 +5,46 @@ class DashboardsController < ApplicationController
        ## @dashboard = Dashboard.new
         @user = current_user
         @user_preferences = UserPreference.find_by_id(@user.id)
-        @forecast = api_call()
-        @forecast_current = @forecast['current']
-        @forecast_hourly = []
-        count = 2
-        @forecast['hourly'].each do |forecast|
-            count = count + 1
-            if count == 3
-                @forecast_hourly << forecast
-                count = 0
-            end
+        if @user_preferences.geocoded?
+          @forecast = api_call()
+          @forecast_current = @forecast['current']
+          @forecast_hourly = []
+          count = 2
+          @forecast['hourly'].each do |forecast|
+              count = count + 1
+              if count == 3
+                  @forecast_hourly << forecast
+                  count = 0
+              end
+          end
+          @forecast_daily = @forecast['daily']
+        else
+          redirect_to preferences_path, notice: 'Please enter your location'
         end
-        @forecast_daily = @forecast['daily']
     end
 
     def forecast
       @user = current_user
+      @user_preferences = UserPreference.find_by_id(@user.id)
+      if @user_preferences.geocoded?
       @forecast = api_call()
       @forecast_hourly = @forecast['hourly']
       @forecast_daily = @forecast['daily']
+    else
+      redirect_to preferences_path, notice: 'Please enter your location'
+    end
     end
 
     def daily
       @user = current_user
+      @user_preferences = UserPreference.find_by_id(@user.id)
+      if @user_preferences.geocoded?
       @forecast = api_call()
       @forecast_hourly = @forecast['hourly']
       @forecast_daily = @forecast['daily']
+    else
+      redirect_to preferences_path, notice: 'Please enter your location'
+    end
     end
 
     private
